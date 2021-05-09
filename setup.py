4340fb086e7f35ff7868dac515981d3cc74a568f
Eduardo Fillipe da Silva Reis <eduardo.fillipe.ext@conductor.com.br>
2021-05-09 14:33:59 -0300

    Added github's url.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='imgx',
-    python_requires='>=3.9',
-    license='GPLv3',
-    version='1.0.0',
     description='A image processing framework built on top of numpy.',
+    version='1.0.0',
+    license='GPLv3',
     author='Eduardo Fillipe da Silva Reis',
     author_email='eduardo556@live.com',
+    url='https://github.com/eduardo-fillipe/imgx-framework',
     packages=['imgx'],
+    python_requires='>=3.9',
     install_requires=['numpy', 'matplotlib', 'seaborn']
 )
