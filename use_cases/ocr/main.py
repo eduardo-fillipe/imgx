1893467f63bbead36b4fd32a5da46b14f2f884b1
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2021-05-27 06:54:24 -0300

    Driver OCR code refactored.

--- use_cases/ocr/main.py
+++ use_cases/ocr/main.py
@@ -1,15 +1,7 @@
-import numpy as np
-
 from imgx.io.load import load_as_ppm
 from imgx.io.save import save_as_ppm
 from imgx.io import visualization as vs
-from imgx.types.image import Image
 from imgx.mask.masks import MedianSpatialMask, OverlapStrategy
-
-
-def remove_noise(img: Image) -> Image:
-    pass
-
 
 if __name__ == '__main__':
     normal_img = load_as_ppm('image_samples/lorem_s12_c02_just.pbm')
@@ -18,6 +10,7 @@ if __name__ == '__main__':
                                                                   params={'order': 70}))
 
     save_as_ppm(img_just_smooth, 'img_just_smooth.pbm')
+
     vs.plot_images([vs.PrintableAxe(normal_img, "Normal Image"),
                     vs.PrintableAxe(img_just_noise, "Noised Image"),
                     vs.PrintableAxe(img_just_smooth, "Smoothed Image")], markers=True)
