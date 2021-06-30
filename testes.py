7d1fabb1546c021abadbae8cfea9ae73f72684d4
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2021-06-29 21:11:15 -0300

    Adicionada operação de erosão.

--- testes.py
+++ testes.py
@@ -1,18 +1,29 @@
 import numpy as np
 import imgx.morphology.operations as op
+import time
 
 se = [[0, 1, 0],
       [1, 1, 1],
       [0, 1, 0]]
 
+# se = [[0, 1, 0, 0, 0],
+#       [1, 1, 1, 0, 0],
+#       [0, 1, 0, 0, 0],
+#       [0, 1, 0, 0, 0],
+#       [0, 1, 0, 0, 0]]
+
 img = [[0, 0, 0, 0, 0, 0],
-       [0, 1, 0, 1, 1, 0],
-       [0, 1, 1, 0, 0, 0],
-       [0, 1, 1, 1, 0, 1],
-       [1, 0, 0, 0, 1, 0],
-       [0, 1, 0, 1, 0, 1]]
+       [0, 1, 1, 1, 1, 0],
+       [0, 1, 1, 1, 1, 0],
+       [0, 1, 1, 0, 1, 0],
+       [0, 1, 1, 1, 1, 0],
+       [0, 0, 0, 0, 0, 0]]
 
-img = np.zeros(shape=(3000, 1500), dtype=int)
+img = np.array(img, dtype=int)
 
+# img = np.zeros(shape=(3000, 1500), dtype=int)
 
-print(op.dilate(img, np.array(se, dtype=int)))
+t1 = time.time()
+print(op.erode(img, np.array(se, dtype=int)))
+t2 = time.time()
+print(t2 - t1)
