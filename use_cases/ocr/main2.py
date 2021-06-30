b80753dcac6b12ba4c08786adce861535739beed
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2021-06-30 20:03:31 -0300

    Contando colunas.

--- use_cases/ocr/main2.py
+++ use_cases/ocr/main2.py
@@ -22,8 +22,36 @@ if __name__ == '__main__':
 
     # se = np.array(se, dtype=int)
 
-    se = np.ones(shape=(35, 19), dtype=int)
+    se = np.ones(shape=(79, 19), dtype=int)
 
-    vs.plot_images([
-         vs.PrintableAxe(img, "Normal"),
-                    vs.PrintableAxe(img.erode(np.ones(shape=(3, 3), dtype=int)).dilate(se).dilate(se).dilate(se), "Dilate")], markers=True)
+    img = img.erode(np.ones(shape=(3, 3), dtype=int)).dilate(se).dilate(se).dilate(se)
+
+    # vs.plot_images([
+    #      vs.PrintableAxe(img, "Normal"),
+    #                 vs.PrintableAxe(img, "Dilate")], markers=True)
+
+    column_count = 0
+    p = 0
+    while True:
+        if np.any(img.data[:, p] == 1):
+            column_count += 1
+            break
+        p += 1
+
+    print('p ', p)
+
+    last = 1
+    for j in range(p, img.data.shape[1]):
+        if last == 0 and np.any(img.data[:, j]):
+            column_count += 1
+            last = 1
+        elif np.alltrue(img.data[:, j] == 0):
+            last = 0
+        else:
+            last = 1
+
+    print(column_count)
+
+
+
+
